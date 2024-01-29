6638511cc16cd9d3135c5bdc4195c9915b2ccc42
MOHAMMAD IQBAL MAULANA <firmanpeman@gmail.com>
2024-01-29 16:29:55 +0700

    connect ke database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import { mongoose } from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000')
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Database terhubung')
+}).catch((err) => {
+    console.log(err)
+    console.log('Database tidak terhubung')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`)
 })
