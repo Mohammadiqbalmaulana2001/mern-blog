57f20618b6a02e6e7ca5e41ec5ac25ecbe4e7213
MOHAMMAD IQBAL MAULANA <firmanpeman@gmail.com>
2024-01-29 19:24:23 +0700

    tes api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { mongoose } from 'mongoose'
 import dotenv from 'dotenv'
+import userRoute from './routes/user.route.js'
 
 const app = express()
+app.use(express.json())
 dotenv.config()
-
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
@@ -15,6 +16,8 @@ mongoose.connect(process.env.DB_URL, {
     console.log(err)
     console.log('Database tidak terhubung')
 })
+
+app.use('/api/users', userRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`)
