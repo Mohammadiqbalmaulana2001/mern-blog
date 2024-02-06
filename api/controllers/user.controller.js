516acf0d10ced2c44c3f61a839d85f6f0090d83e
MOHAMMAD IQBAL MAULANA <firmanpeman@gmail.com>
2024-02-06 18:17:03 +0700

    membuat route api get users

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -64,3 +64,40 @@ export const signout = async (req, res, next) => {
         next(HandleError(res, 400, error.message))
     }
 }
+
+export const getusers = async (req, res, next) => {
+    if(!req.user.isAdmin){
+        return next(HandleError(res, 401, "Anda tidak diperbolehkan melihat semua pengguna"))
+    }
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0
+        const limit = parseInt(req.query.limit) || 10
+        const sortDirection = req.query.order === 'asc' ? 1 : -1
+
+        const users = await User.find().sort({createdAt: sortDirection}).skip(startIndex).limit(limit)
+        
+        const usersWithoutPassword = users.map((user) => {
+            const { password, ...rest } = user._doc
+            return rest
+        })
+
+        const totalUsers = await User.countDocuments()
+        const now = new Date()
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        )
+        const lastMonthUsers = await User.countDocuments({
+            createdAt:{$gte: oneMonthAgo}
+        })
+        
+        res.status(200).json({
+            users: usersWithoutPassword,
+            totalUsers,
+            lastMonthUsers
+        })
+    } catch (error) {
+        next(HandleError(res, 400, error.message))
+    }
+}
