15c2c4a3cea40945ae92eb5c6501e94e205cf067
MOHAMMAD IQBAL MAULANA <firmanpeman@gmail.com>
2024-02-06 12:44:50 +0700

    menambahkan fungsi post lebih banyak

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 export default function DashPost() {
   const {currentUser} = useSelector((state) => state.user)
   const [userPosts,setUserPosts] = useState([])
+  const [showMore,setShowMore] = useState(true)
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -13,6 +14,9 @@ export default function DashPost() {
         const data = await res.json()
         if(res.ok){
           setUserPosts(data.post)
+          if(data.post.length < 9){
+            setShowMore(false)
+          }
         }
       } catch (error) {
         console.log(error)
@@ -22,6 +26,22 @@ export default function DashPost() {
       fetchPosts()
     }
   },[currentUser._id])
+
+  const handleLebihBanyak = async () => {
+    const startIndex = userPosts.length
+    try {
+      const res = await fetch(`/api/posts/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+      const data = await res.json()
+      if(res.ok){
+        setUserPosts((prev) => [...prev, ...data.post])
+        if(data.post.length < 9){
+          setShowMore(false)
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 srollbar-thumb-slate-300 dark:srollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -58,6 +78,11 @@ export default function DashPost() {
             </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button className="w-full text-teal-400 self-center text-sm py-7 " onClick={handleLebihBanyak}>
+              Lebih banyak
+            </button>
+          )}
         </>
       ) : (
         <p>Tidak ada postingan</p>
