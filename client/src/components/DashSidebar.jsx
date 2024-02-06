ee34048a5dede76a1da252b0e0837c90205118a9
MOHAMMAD IQBAL MAULANA <firmanpeman@gmail.com>
2024-02-06 09:00:01 +0700

    menambahkan section dasboard post ke client

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,11 +1,13 @@
 import {Sidebar} from 'flowbite-react'
-import {HiUser , HiArrowSmRight} from 'react-icons/hi'
+import {HiUser , HiArrowSmRight, HiDocumentText} from 'react-icons/hi'
 import { useEffect, useState } from "react"
 import {Link, useLocation} from 'react-router-dom'
 import { signOutSuccess } from '../redux/user/userSlice'
 import {useDispatch} from 'react-redux'
+import {useSelector} from 'react-redux'
 export default function DashSidebar() {
     const location = useLocation()
+    const {currentUser} = useSelector((state) => state.user)
     const [tab,setTab] =useState('')
     const dispatch = useDispatch()
 
@@ -35,10 +37,17 @@ export default function DashSidebar() {
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
-            <Sidebar.ItemGroup>
-                    <Sidebar.Item as={Link} to='/dashboard?tab=profile' active={tab === 'profile'} icon={HiUser} label={'user'}  labelColor='dark'>
+            <Sidebar.ItemGroup className='flex flex-col gap-1'>
+                    <Sidebar.Item as={Link} to='/dashboard?tab=profile' active={tab === 'profile'} icon={HiUser} label= {currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark'>
                     Profile
                     </Sidebar.Item>
+                    {
+                      currentUser.isAdmin && (
+                        <Sidebar.Item as={Link} to={'/dashboard?tab=posts'} active={tab === 'posts'} icon={HiDocumentText}  >
+                      Posts
+                    </Sidebar.Item>
+                      )
+                    }
 
                     <Sidebar.Item as={Link} to={'/logout'} active icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignOut}>
                     Sign Out
